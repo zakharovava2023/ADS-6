287b888087cdf6be71607eea7cef08c8c8ab561a
anton <ashtanyuk@gmail.com>
2023-01-30 19:50:58 +0300

    Initial commit

---
+++ include/tpqueue.h
@@ -0,0 +1,15 @@
+// Copyright 2022 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+
+template<typename T, int size>
+class TPQueue {
+  // реализация шаблона очереди с приоритетом на кольцевом буфере
+};
+
+struct SYM {
+  char ch;
+  int prior;
+};
+
+#endif  // INCLUDE_TPQUEUE_H_
