a69da71bb156b40cde27d147431f08f3925b29f9
zakharovava2023 <125122557+zakharovava2023@users.noreply.github.com>
2023-04-12 15:47:14 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,12 +1,12 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
+#include <string>
 template<typename T, int size>
 class TPQueue {
 private:
-    T* arr;
-    int first, last, count;
+ T* arr;
+ int first, last, count;
 
 public:
     TPQueue();
